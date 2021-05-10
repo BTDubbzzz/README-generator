59402d7afe03ebbea5d421589d682e1bc05cf80b
BTDubbzzz <btdubbzzz@gmail.com>
2021-05-10 17:03:51 -0500

    setup basic file structure and installed npms

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +9,51 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'username'
+            },
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'username'
+            },
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'username'
+            },
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'username'
+            }
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'username'
+            }
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'username'
+            }
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'username'
+            }
+        ])
+        .then(function(response) {
+            console.log(response);
+            let { username, location, bio, linkedinurl, githuburl } = response;
+            writeToFile(username, location, bio, linkedinurl, githuburl)
+        })
+}
 
 // Function call to initialize app
 init();
